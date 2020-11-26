488a3165407c8b191aec394228697b824e02a7b0
azriel1rf <azriel.1rf@gmail.com>
2020-11-26 12:51:45 +0900

    modified project structure

---
+++ cp_lib/__init__.py
@@ -0,0 +1,12 @@
+from .arithmetic import permutation, factorial, combination
+from .multiset import MultiSet
+from .minmultiset import MinMultiSet, MaxMultiSet
+
+__all__ = [
+    "permutation",
+    "factorial",
+    "combination",
+    "MultiSet",
+    "MinMultiSet",
+    "MaxMultiSet",
+]
