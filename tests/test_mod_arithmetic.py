98917f98384fe07916aafaae2f89f1ff57aec74c
azriel1rf <azriel.1rf@gmail.com>
2020-11-27 01:37:30 +0900

    changed module name

--- tests/test_mod_arithmetic.py
+++ tests/test_mod_arithmetic.py
@@ -1,53 +1,57 @@
-from tiny_algos.mod_arithmetic import permutation, factorial, combination
+from tiny_algos.mod_arithmetic import (
+    mod_permutation,
+    mod_factorial,
+    mod_combination,
+)
 
 DEFAULT_MOD = 10 ** 9 + 7
 
 
 def test_permutation():
-    assert permutation(5, 3) == (5 * 4 * 3) % DEFAULT_MOD
-    assert permutation(10 ** 10, 1) == (10 ** 10) % DEFAULT_MOD
+    assert mod_permutation(5, 3) == (5 * 4 * 3) % DEFAULT_MOD
+    assert mod_permutation(10 ** 10, 1) == (10 ** 10) % DEFAULT_MOD
 
-    assert permutation(1, 1) == 1
-    assert permutation(1, 0) == 1
-    assert permutation(0, 0) == 1
+    assert mod_permutation(1, 1) == 1
+    assert mod_permutation(1, 0) == 1
+    assert mod_permutation(0, 0) == 1
 
     mod = 7
-    assert permutation(5, 3, mod) == (5 * 4 * 3) % mod
-    assert permutation(10 ** 10, 1, mod) == (10 ** 10) % mod
+    assert mod_permutation(5, 3, mod) == (5 * 4 * 3) % mod
+    assert mod_permutation(10 ** 10, 1, mod) == (10 ** 10) % mod
 
-    assert permutation(1, 1, mod) == 1
-    assert permutation(1, 0, mod) == 1
-    assert permutation(0, 0, mod) == 1
+    assert mod_permutation(1, 1, mod) == 1
+    assert mod_permutation(1, 0, mod) == 1
+    assert mod_permutation(0, 0, mod) == 1
 
 
 def test_factorial():
-    assert factorial(5) == (5 * 4 * 3 * 2 * 1) % DEFAULT_MOD
-    assert factorial(DEFAULT_MOD + 1) == 0
-    assert factorial(1) == 1
-    assert factorial(0) == 1
+    assert mod_factorial(5) == (5 * 4 * 3 * 2 * 1) % DEFAULT_MOD
+    assert mod_factorial(DEFAULT_MOD + 1) == 0
+    assert mod_factorial(1) == 1
+    assert mod_factorial(0) == 1
 
     mod = 7
-    assert factorial(5, mod) == (5 * 4 * 3 * 2 * 1) % mod
-    assert factorial(mod + 1, mod) == 0
-    assert factorial(1, mod) == 1
-    assert factorial(0, mod) == 1
+    assert mod_factorial(5, mod) == (5 * 4 * 3 * 2 * 1) % mod
+    assert mod_factorial(mod + 1, mod) == 0
+    assert mod_factorial(1, mod) == 1
+    assert mod_factorial(0, mod) == 1
 
 
 def test_combination():
-    assert combination(5, 3) == ((5 * 4 * 3) // (3 * 2 * 1)) % DEFAULT_MOD
-    assert combination(10 ** 10, 1) == (10 ** 10) % DEFAULT_MOD
+    assert mod_combination(5, 3) == ((5 * 4 * 3) // (3 * 2 * 1)) % DEFAULT_MOD
+    assert mod_combination(10 ** 10, 1) == (10 ** 10) % DEFAULT_MOD
 
-    assert combination(1, 1) == 1
-    assert combination(1, 0) == 1
-    assert combination(0, 0) == 1
+    assert mod_combination(1, 1) == 1
+    assert mod_combination(1, 0) == 1
+    assert mod_combination(0, 0) == 1
 
     mod = 2
-    assert combination(5, 3, mod) == ((5 * 4 * 3) // (3 * 2 * 1)) % mod
+    assert mod_combination(5, 3, mod) == ((5 * 4 * 3) // (3 * 2 * 1)) % mod
     # denominator might be divided by mod
-    assert combination(7, 2, mod) == ((7 * 6) // (2 * 1)) % mod
+    assert mod_combination(7, 2, mod) == ((7 * 6) // (2 * 1)) % mod
     mod = 7
-    assert combination(10 ** 10, 1, mod) == (10 ** 10) % mod
+    assert mod_combination(10 ** 10, 1, mod) == (10 ** 10) % mod
 
-    assert combination(1, 1, mod) == 1
-    assert combination(1, 0, mod) == 1
-    assert combination(0, 0, mod) == 1
+    assert mod_combination(1, 1, mod) == 1
+    assert mod_combination(1, 0, mod) == 1
+    assert mod_combination(0, 0, mod) == 1
--- tiny_algos/__init__.py
+++ tiny_algos/__init__.py
@@ -1,11 +1,11 @@
-from .mod_arithmetic import permutation, factorial, combination
+from .mod_arithmetic import mod_permutation, mod_factorial, mod_combination
 from .multiset import MultiSet
 from .minmultiset import MinMultiSet, MaxMultiSet
 
 __all__ = [
-    "permutation",
-    "factorial",
-    "combination",
+    "mod_permutation",
+    "mod_factorial",
+    "mod_combination",
     "MultiSet",
     "MinMultiSet",
     "MaxMultiSet",
