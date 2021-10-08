fae320bfde25a9aec1068432b8ee628c9a8bf8af
azriel1rf <azriel.1rf@gmail.com>
2021-10-08 13:06:26 +0900

    parametarized testcases

--- tests/test_multiset.py
+++ tests/test_multiset.py
@@ -1,71 +1,121 @@
-from tiny_algos.multiset import MultiSet
-from tiny_algos.minmultiset import MinMultiSet, MaxMultiSet
 from copy import deepcopy
+from itertools import product
+
+import pytest
+from tiny_algos.minmultiset import MaxMultiSet, MinMultiSet
+from tiny_algos.multiset import MultiSet
+
+PINF = 9223372036854775807  # 2 ** 63 - 1
+NINF = -PINF
+MIN_DEFAULT = PINF
+MAX_DEFAULT = NINF
 
 
-test_bases = [
-    [0, 1, 1, 2],
-    [0, 1, 1, 2],
-    [2, 4, 0, 2],
+class ScenarioWrapper:
+    def __init__(self, base, action, description):
+        self.base = base
+        self.action = action
+        self.description = description
+
+    def __repr__(self):
+        return f'"{self.description}"'
+
+
+scenarios = [
+    ScenarioWrapper(
+        base=[],
+        action=[
+            ("add", 1),
+            ("add", 2),
+            ("add", -1),
+        ],
+        description="construct with empty list",
+    ),
+    ScenarioWrapper(
+        base=[1, -1],
+        action=[
+            ("remove", 1),
+            ("remove", -1),
+            ("add", 1),
+            ("add", -1),
+        ],
+        description="Remove all and add",
+    ),
+    ScenarioWrapper(
+        base=[0, 1, 1, 2],
+        action=[
+            ("add", 3),
+            ("add", 2),
+            ("add", -1),
+            ("add", -2),
+        ],
+        description="add",
+    ),
+    ScenarioWrapper(
+        base=[0, 1, 1, 2],
+        action=[
+            ("remove", 1),
+            ("remove", 2),
+            ("remove", 0),
+        ],
+        description="remove",
+    ),
+    ScenarioWrapper(
+        base=[2, 4, 0, 2],
+        action=[
+            ("add", 3),
+            ("remove", 4),
+            ("add", 1),
+            ("remove", 0),
+            ("add", 1),
+            ("add", 3),
+            ("remove", 1),
+            ("remove", 3),
+            ("remove", 1),
+            ("remove", 2),
+        ],
+        description="add and remove",
+    ),
 ]
 
-episodes = [
-    [
-        ("add", 3),
-        ("add", 2),
-        ("add", -1),
-        ("add", -2),
-    ],
-    [
-        ("remove", 1),
-        ("remove", 2),
-        ("remove", 0),
-    ],
-    [
-        ("add", 3),
-        ("remove", 4),
-        ("add", 1),
-        ("remove", 0),
-        ("add", 1),
-        ("add", 3),
-        ("remove", 1),
-        ("remove", 3),
-        ("remove", 1),
-        ("remove", 2),
-    ],
+
+class MultiSetWrapper:
+    def __init__(self, ms_cls, extremes):
+        self.ms_cls = ms_cls
+        self.extremes = extremes
+
+    def __repr__(self):
+        return self.ms_cls.__name__
+
+
+multisets = [
+    MultiSetWrapper(MultiSet, extremes=("min", "max")),
+    MultiSetWrapper(MinMultiSet, extremes=("min")),
+    MultiSetWrapper(MaxMultiSet, extremes=("max")),
 ]
 
 
-def test_multiset():
-    for test_base, episode in zip(test_bases, episodes):
-        verify(MultiSet, test_base, episode, extremes=("min", "max"))
-
-
-def test_minmultiset():
-    for test_base, episode in zip(test_bases, episodes):
-        verify(MinMultiSet, test_base, episode, extremes=("min"))
-
-
-def test_maxmultiset():
-    for test_base, episode in zip(test_bases, episodes):
-        verify(MaxMultiSet, test_base, episode, extremes=("max"))
-
-
-def verify(multiset_class, test_base, episode, extremes=("min", "max")):
-    test_base = deepcopy(test_base)
-    episode = deepcopy(episode)
-    test_list = list(test_base)
-    test_ms = multiset_class(list(test_base))
+@pytest.mark.parametrize(
+    ["scenario_wrapper", "multiset_wrapper"],
+    product(scenarios, multisets),
+)
+def test_multiset(scenario_wrapper, multiset_wrapper):
+    ms_cls = multiset_wrapper.ms_cls
+    extremes = multiset_wrapper.extremes
+    base = deepcopy(scenario_wrapper.base)
+    action = deepcopy(scenario_wrapper.action)
+    test_list = list(base)
+    test_ms = ms_cls(list(base))
 
     def check_extremes():
         if "min" in extremes:
-            assert test_ms.min == min(test_list)
+            assert test_ms.min == min(test_list, default=MIN_DEFAULT)
         if "max" in extremes:
-            assert test_ms.max == max(test_list)
+            assert test_ms.max == max(test_list, default=MAX_DEFAULT)
 
     check_extremes()
 
-    for action, value in episode:
+    for action, value in action:
         if action == "add":
             test_list.append(value)
             test_ms.add(value)
